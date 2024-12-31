94b034b7bf82cf172dc8d5ab162a62bd6ce259a1
Sonalkumar Singh <132155283+sonal017@users.noreply.github.com>
2024-12-31 17:42:23 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import IMG from ""../../components/img/adminpic.jpeg";
+import IMG from "../components/img/adminpic.jpeg";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
